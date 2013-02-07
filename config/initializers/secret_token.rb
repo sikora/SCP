613ebcb4fb7714464b1ca5931118116f2c732627
Carlos Wherbet <carloswherbet@gmail.com>
2013-02-06 23:04:33 -0300

    Autenticacao, arquivos de traducao do devise, bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scp::Application.config.secret_token = '67b311d021a4b6dca56f2259e05717eeb7b060130a1b76ba4db3a2b29ae633999c6d5db8af5ae28f00620022c838b25e770c8245baf121259cdefde53f211496'
