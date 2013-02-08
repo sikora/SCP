cd1c10142acf3fb9ba8bcd97bfa1b15e215f3e9a
Rodolfo Sikora <sikora@gmail.com>
2013-02-08 15:22:24 -0300

    fixes para rodar em produção

--- app/models/cargocategoria.rb
+++ app/models/cargocategoria.rb
@@ -1,7 +1,7 @@
 class Cargocategoria < ActiveRecord::Base
   attr_accessible :id_cargo, :id_categoria, :id_contrato, :id_orgao, :valor
-  belgons_to : :cargo, :foreign_key => 'id_cargo'
-  belgons_to : :categoria, :foreign_key => 'id_categoria'
-  belgons_to : :orgao, :foreign_key => 'id_orgao'
-  belgons_to : :contrato, :foreign_key => 'id_contrato'
+  belongs_to :cargo, :foreign_key => 'id_cargo'
+  belongs_to :categoria, :foreign_key => 'id_categoria'
+  belongs_to :orgao, :foreign_key => 'id_orgao'
+  belongs_to :contrato, :foreign_key => 'id_contrato'
 end
