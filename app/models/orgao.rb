f7cff6d0d4e8266fe805d558ff6f9f075f098e5e
Rodolfo Sikora <sikora@gmail.com>
2013-02-08 14:09:12 -0300

    fixes em orgao

--- app/models/orgao.rb
+++ app/models/orgao.rb
@@ -1,5 +1,6 @@
 class Orgao < ActiveRecord::Base
-  attr_accessible :dt_fim, :dt_inicio, :id_lei, :nm_orgao
+  attr_accessible :dt_fim, :dt_inicio, :lei_id, :nm_orgao
   has_many :cargocategorias
-  has_many :lotacoes
+  has_many :lotacoes, foreign_key: "id_orgao"
+  belongs_to :lei
 end
