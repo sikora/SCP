a44e1b087d5b47ddcb479912085f8bd069b54f39
Rodolfo Sikora <sikora@gmail.com>
2013-02-11 14:16:42 -0300

    fixing idpolitico and contratacoes

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,5 +1,5 @@
 class Pessoa < ActiveRecord::Base
-  attr_accessible :bairro, :cep, :cpf, :dtnasc, :endereco, :idpolitico, :nacionalidade, :naturalidade, :nmmae, :nmpai, :nmpessoa, :obs, :sexo, :telefone
+  attr_accessible :bairro, :cep, :cpf, :dtnasc, :endereco, :indicador_id, :nacionalidade, :naturalidade, :nmmae, :nmpai, :nmpessoa, :obs, :sexo, :telefone
   has_many :contratacoes
-  belongs_to :indicador ,:foreign_key => 'idpolitico' 
+  belongs_to :indicador 
 end
