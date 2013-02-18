aa2240971a485ba590da1e4091e77219d6b99d1e
Carlos Wherbet <carloswherbet@gmail.com>
2013-02-18 16:06:29 -0300

    relacionamento cargo
    relacionamento cargo

--- app/models/cargo.rb
+++ app/models/cargo.rb
@@ -2,7 +2,7 @@ class Cargo < ActiveRecord::Base
   audited
   validates_presence_of :nm_cargo
   attr_accessible :dt_fim, :dt_inicio, :nm_cargo
-  has_many :cargocategorias
+  has_many :cargo_categorias
 
 
 end
