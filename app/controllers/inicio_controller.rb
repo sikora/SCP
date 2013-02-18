df142a17a0d18a423d64a9ce88c1e7a4b1b59d84
arthurmo <arthurmolina@yahoo.com.br>
2013-02-18 16:04:46 -0300

    "Pessoas sem contrato" added to home

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -1,6 +1,10 @@
 #encoding: utf-8
 class InicioController < ApplicationController
   def index
+  	@pessoas = Pessoa.find(:all, :limit => 10,
+            :joins => "LEFT JOIN contratacoes ON contratacoes.pessoa_id = pessoas.id" ,
+            :select => "pessoas.nm_pessoa as nm_pessoa, pessoas.id as id",
+            :conditions => "contratacoes.id IS NULL")
 
   end
 end
