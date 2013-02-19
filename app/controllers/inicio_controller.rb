3efd9dad0a6e418a612f542f7c44de5f5a6c1873
Carlos Wherbet <carloswherbet@gmail.com>
2013-02-19 09:54:06 -0300

    Ultimas contratacoes Dashboard

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -1,10 +1,12 @@
 #encoding: utf-8
 class InicioController < ApplicationController
   def index
-  	@pessoas = Pessoa.find(:all, :limit => 10,
+  	@pessoas = Pessoa.find(:all, :limit => 3,
             :joins => "LEFT JOIN contratacoes ON contratacoes.pessoa_id = pessoas.id" ,
             :select => "pessoas.nm_pessoa as nm_pessoa, pessoas.id as id",
             :conditions => "contratacoes.id IS NULL")
+
+  	@contratacoes =  Contratacao.order("created_at DESC").limit(3)
 
   end
 end
